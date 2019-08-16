41f149e2261c0f8c7bb5689a99ef8bf6866ac8fb
Ivan Fuentes <ivanfuentesglez@gmail.com>
2019-08-16 13:26:08 -0500

    se agregaron 2 sliders en la pagina de inicio

---
+++ js/init.carousel.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	$('.hero-carousel').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    nav:true,
+	    autoplay:true,
+    	autoplayTimeout:1000,
+    	autoplayHoverPause:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:2
+	        },
+	        1000:{
+	            items:3
+	        }
+	    }
+	})
+
+
+	$('.testimonial-carousel').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    nav:true,
+	    autoplay:true,
+    	autoplayTimeout:1000,
+    	autoplayHoverPause:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:2
+	        },
+	        1000:{
+	            items:3
+	        }
+	    }
+	})
+});
